31759db77b8cf57c94721fb4d2a2a4102ed6a45b
Erik Remington Lee <anathema8808@yahoo.com>
2022-11-21 01:43:54 -0500

    service stack working

---
+++ Hades.ServiceInterface/MyServices.cs
@@ -0,0 +1,12 @@
+using ServiceStack;
+using Hades.ServiceModel;
+
+namespace Hades.ServiceInterface;
+
+public class MyServices : Service
+{
+    public object Any(Hello request)
+    {
+        return new HelloResponse { Result = $"Hello, {request.Name}!" };
+    }
+}
