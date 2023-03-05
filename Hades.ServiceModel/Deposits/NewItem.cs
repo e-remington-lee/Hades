6897b305958d5c7001a3fdc50b6c6a01cb5e3929
Erik Remington Lee <anathema8808@yahoo.com>
2023-03-05 18:29:04 -0500

    adding service test package

--- Hades.ServiceModel/Deposits/NewItem.cs
+++ Hades.ServiceModel/Deposits/NewItem.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Hades.ServiceModel.DTO
+namespace Hades.ServiceModel.Deposits
 {
-    public class Payment
+    public class NewItem
     {
     }
 }
